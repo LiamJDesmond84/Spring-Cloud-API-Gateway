1f773019d41bfddbb9b5450ac02c6478f917636f
Liam Desmond <liamjdesmond84@gmail.com>
2022-03-21 15:56:19 -0500

    updating fallback

--- src/main/java/com/liam/cloud/gateway/FallBackMethodController.java
+++ src/main/java/com/liam/cloud/gateway/FallBackMethodController.java
@@ -8,12 +8,11 @@ public class FallBackMethodController {
 	
 	@GetMapping("/userServiceFallBack")
     public String userServiceFallBackMethod() {
-        return "User Service is taking longer than Expected.";
+        return "User Service is not responding.";
     }
 
     @GetMapping("/departmentServiceFallBack")
     public String departmentServiceFallBackMethod() {
-        return "Department Service is taking longer than Expected." +
-                " Please try again later";
+        return "Department Service is not responding.";
     }
 }
